a35cac22e29ad6a952a80b1eb868ec04cb88566f
unknown <martinperez9713@gmail.com>
2023-11-29 23:19:36 -0300

    clase 3: añadiendo estilos inline, con archivo css externo y evento onClick

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-  
+  const saludo = "Hola Mundo";
+
+  const saludando = ()=>{
+    alert("Hiciste Click")
+  }
+
   return (
-    <>
-      <div>
-        <h1>Bienvenidos a React</h1>
-      </div>
-    </>
-  )
+    <div id="app">
+      <img src="/img/react.png" alt="" />
+      <p>aasdasd</p>
+      <h1> {saludo} </h1>
+      <button onClick={ saludando }>Clickeame</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
