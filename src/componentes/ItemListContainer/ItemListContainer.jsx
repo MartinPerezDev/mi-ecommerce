61c1a94bf920e179d4e3e89d0973ed294c6cf65f
unknown <martinperez9713@gmail.com>
2023-12-12 20:55:37 -0300

    clase 7: probando todos los verbos en una api local

--- src/componentes/ItemListContainer/ItemListContainer.jsx
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -1,29 +1,51 @@
 import { useEffect, useState } from "react";
 
-import obtenerProductos from "../utilidades/data";
 import ItemList from "../ItemList/ItemList";
+import { getProductos, getProductosPorId, postAñadirProducto, putModificarProducto, deleteProducto } from "../utilidades/fetchApi"
 
-import "./ItemListContainer.css"
+import "./ItemListContainer.css";
 
 const ItemListContainer = ({ saludo }) => {
   const [productos, setProductos] = useState([]);
 
+  const nuevoProducto = {
+    nombre: "Auriculares Float Run",
+    descripcion: "Este es el mejor teclado del mundo",
+    stock: 6,
+    precio: 17000,
+    imagen: "/img/auriculares-Float-Run.webp",
+    categoria: "auriculares"
+  }
+
+  const clickEvento = ()=>{
+
+    //Añadiendo un producto nuevo
+    /*
+    postAñadirProducto(nuevoProducto)
+      .then((data)=> setProductos(data))
+      putModificarProducto(1, { precio: 25000, nombre: "Auto 0km" })
+      .then((data)=> setProductos(data))
+      */
+    deleteProducto(1)
+      .then((data)=> setProductos(data))
+  }
+
   useEffect(() => {
-    obtenerProductos
-      .then((respuesta) => {
-        setProductos(respuesta);
-      })
-      .catch((error) => {
-        console.log(error);
-      })
-      .finally(() => {
-        console.log("finalizo la promesa");
-      });
+
+    //traemos todos los productos
+    getProductos()
+      .then((data)=> setProductos(data))
+    
+    //traemos un solo producto por id
+    getProductosPorId(1)
+      .then((data)=> console.log(data))
+      
   }, []);
 
   return (
     <div className="item-list-container">
       <p className="saludo">{saludo}</p>
+      <button onClick={clickEvento} >Click</button>
       <ItemList productos={productos} />
     </div>
   );
