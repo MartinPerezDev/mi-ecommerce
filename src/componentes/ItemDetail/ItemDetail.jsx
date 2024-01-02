befe3f2643c033d6ec4ba32271859d1e9be8bb7d
EfimeroM <martinperez9713@gmail.com>
2024-01-02 13:22:40 -0300

    desafio ItemCount Avanzado

--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
+
+import { Link } from "react-router-dom";
+import ItemCount from "../ItemCount/ItemCount";
+
 import "./ItemDetail.css";
 
 const ItemDetail = ({ producto }) => {
+  const [toggle, setToggle] = useState(false);
+
+  const agregarAlCarrito = (contador) => {
+    console.log(contador);
+    setToggle(true);
+  };
+
   return (
     <div className="item-detail">
       <img className="imagen" src={producto.imagen} alt={producto.nombre} />
@@ -8,6 +20,21 @@ const ItemDetail = ({ producto }) => {
         <h2>{producto.nombre}</h2>
         <p>{producto.descripcion}</p>
         <p>${producto.precio}</p>
+        {toggle ? (
+          <>
+            <Link className="button-terminar" to="/carrito">
+              Terminar mi compra
+            </Link>
+            <Link className="button-terminar" to="/">
+              Seguir comprando
+            </Link>
+          </>
+        ) : (
+          <ItemCount
+            stock={producto.stock}
+            agregarAlCarrito={agregarAlCarrito}
+          />
+        )}
       </div>
     </div>
   );
